0a5c13b822fc3fbaa28b2f3dcef25f544da9d47a
gauravkmr0110 <kmrgaurav0101@gmail.com>
2026-04-05 22:42:32 +0530

    Discount Coupon using Strategy

--- Practice Questions/DiscountCoupon.java
+++ Practice Questions/DiscountCoupon.java
@@ -7,8 +7,9 @@ Design a system where:
     - multiple coupons can also be applied sequentially
  */
 import java.util.*;
+
 // ----------- entities ------
-enum Itemcategory{
+enum ProductCategory{
     ELECTRONICS,
     CLOTHING,
     GROCERRY,
@@ -16,24 +17,163 @@ enum Itemcategory{
     FURNITURE,
     OTHER
 }
-// cart item class
-class CartItem{
+
+// prodcut class
+class Product{
     String name;
-    Itemcategory itemcategory;
-    int price ;
+    ProductCategory productCategory;
+    double price ;
+    int quantity;
+
+    Product(String name,double price, int quantity,ProductCategory category){
+        this.name = name;
+        this.productCategory = category;
+        this.price = price;
+        this.quantity= quantity;
+    }
+
+    double getPrice(){
+        return price*quantity;
+    }
+
+}
+
+// for effcetive pricing we need cartItem
+
+class CartItem{
+    Product product;
+    double effectivePrice;
+
+    CartItem(Product p){
+        this.product = p;
+        this.effectivePrice = p.getPrice();
+    }
+
+    ProductCategory getCartItemCategory(){
+        return product.productCategory;
+    }
+
+
+
 }
 
 // cart class
+class ShoppingCart{
+    List<CartItem> itemList;
 
-class Cart{
-    List<CartItem> items
+    public ShoppingCart(){
+        this.itemList = new ArrayList<>();
+    }
+
+    public void addToCart(Product product){
+        itemList.add(new CartItem(product));
+    }
+
+    public double getCartValue(){
+        double total = 0;
+        for(CartItem item:itemList){
+           total+=item.effectivePrice;
+        }
+        return total;
+    }
+
+}
+
+// -------- DIFFERENT STRATEGIES FOR COUPONS ------
+
+interface CouponStrategy{
+    void applyCoupon(ShoppingCart cart);
+}
+
+// FLAT COUPOUN ON CART VALUE, EACH PRODUCT PRICE WILL CHANGE ACCORDINGLY
+class FlatDiscountALL implements CouponStrategy{
+    private double discount;
+    FlatDiscountALL(double discount){
+        this.discount=discount;
+    }
+
+    @java.lang.Override
+    public void applyCoupon(ShoppingCart cart) {
+        // contribution of each product in flat discount
+        double cartValue = cart.getCartValue();
+        if(cartValue==0){
+            return ;
+        }
+        for(CartItem item: cart.itemList){
+            double itemDiscount = (item.effectivePrice/cartValue)*discount;
+            item.effectivePrice-=itemDiscount;
+            item.effectivePrice=Math.max(0,item.effectivePrice);
+        }
+
+    }
+}
+
+// PERCENTAGE DISCOUNT
+class PercentDiscountALL implements CouponStrategy{
+    private double discountPercent;
+    PercentDiscountALL(double discountPercent){
+        this.discountPercent = discountPercent;
+    }
+
+    @java.lang.Override
+    public void applyCoupon(ShoppingCart cart) {
+        for(CartItem item: cart.itemList){
+            double itemDiscount = (item.effectivePrice*discountPercent)/100.0;
+            item.effectivePrice-=itemDiscount;
+            item.effectivePrice=Math.max(0,item.effectivePrice);
+
+        }
+
+    }
+}
+
+// DISCOUNT ON CATEGORY can have both flat or percent we will impleemnt percent only
+class PercentDiscountCategory implements CouponStrategy{
+    private double discountPercent;
+    ProductCategory discountCategory;
+
+    PercentDiscountCategory(double discountPercent, ProductCategory discountCategory){
+        this.discountPercent = discountPercent;
+        this.discountCategory = discountCategory;
+    }
+
+    @java.lang.Override
+    public void applyCoupon(ShoppingCart cart) {
+        for(CartItem item: cart.itemList){
+
+            if(item.getCartItemCategory() == discountCategory) {
+                item.effectivePrice -= (discountPercent * item.effectivePrice) / 100.0;
+                item.effectivePrice = Math.max(0,item.effectivePrice);
+            }
+        }
+
+    }
 }
 
 
-//
-
-public class DiscountCoupon {
+public class DiscountCoupon{
     public static void main(String args[]){
+
+        ShoppingCart cart = new ShoppingCart();
+
+        Product p1 = new Product("Laptop",50000,1,ProductCategory.ELECTRONICS);
+
+        Product p2 = new Product("Shirt",2000,3,ProductCategory.CLOTHING);
+
+        cart.addToCart(p1);
+        cart.addToCart(p2);
+
+        double cartValue = cart.getCartValue();
+
+        // APPLY COUPONS
+        CouponStrategy coupon1 = new PercentDiscountALL(10.0);
+        CouponStrategy coupon2 = new PercentDiscountCategory(15,ProductCategory.ELECTRONICS);
+        CouponStrategy coupon3 = new FlatDiscountALL(1000);
+
+        coupon1.applyCoupon(cart);
+        coupon2.applyCoupon(cart);
+        coupon3.applyCoupon(cart);
+        System.out.println("Final cart value is: " + cart.getCartValue());
 
     }
 }
